34d5c68ea8468850bda343b368392911b2a4aa84
Tharakai <tharakai@freshdesk.com>
2015-08-17 13:56:15 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScoreBoard::Application.config.secret_token = 'bd08f6c1f7c9f5b6332d913d19b6600e1ae385893e9e316c8e3f317f4e0a656232a78a9845c22d1ff9dca1be7e319537fb0085541af098087c1af5d2c3d6d287'
